c2601b3c45d7c371f372bd819bc9dc6759e6f3bc
Urrea <Surrea1411@gmail.com>
2023-09-09 21:51:22 -0500

    Factorial

---
+++ Factorial/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.White;
+Console.Clear();
+bool next;
+
+do
+{
+    var n = RequestInt("n? ");
+    var fact = Factorial(n);
+    Console.WriteLine($"{n}! = {fact}");
+    next = RequestBool("Do you want to calculate another factorial [y/n]? ");
+} while(next);
+
+bool RequestBool(string message)
+{
+    Console.Write(message);
+    var response = Console.ReadLine();
+    if (response!.ToLower() == "y")
+    {
+        return true;
+    }
+    return false;
+}
+
+double Factorial(int n)
+{
+    // Implement yor logic here
+    return 0;
+}
+
+int RequestInt(string message)
+{
+    Console.Write(message);
+    var inputString = Console.ReadLine();
+    try
+    {
+        return Convert.ToInt32(inputString);
+    }
+    catch (Exception)
+    {
+        throw new Exception("You must enter a valid integer number.");
+    }
+}
